e3360507fa72a66cded4206d6d46c03fc59c5d16
L Laniewski-Wollk <llaniewski@meil.pw.edu.pl>
2017-11-24 15:57:24 +1000

    Adding worker_spawn

---
+++ example/worker_spawn.cpp
@@ -0,0 +1,27 @@
+#include "../MPMD.hpp"
+
+int main(int argc, char *argv[])
+{
+   MPMDHelper MPMD;
+   
+   MPI_Init(&argc, &argv);
+   
+   std::vector<int> excl;
+   excl.push_back(0); // Exclude process 0 from the working group.
+   
+   MPMD.Init(MPI_COMM_WORLD, "WITH_WORKERS", excl);
+   
+   for (int i=1; i<argc; i++) {
+      int n = atoi(argv[i]);
+      i++;
+      if (i >= argc) return -1;
+      if (MPMD.in_work) {
+         MPMD.Spawn(argv[i], MPI_ARGV_NULL, n, MPI_INFO_NULL, true);
+      }
+   }
+
+   MPMD.Identify();
+
+   MPI_Finalize();
+   return 0;
+}
